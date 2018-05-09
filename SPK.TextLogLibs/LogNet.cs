244428a62e9f950f2d58463c8a8eb6f75b1902b9
IAMSUTHICHA <suthicha@outlook.com>
2018-05-09 11:11:33 +0700

    add trash method

--- SPK.TextLogLibs/LogNet.cs
+++ SPK.TextLogLibs/LogNet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace SPK.TextLogLibs
 {
@@ -16,16 +17,36 @@ namespace SPK.TextLogLibs
             writeLog(LogType.ERROR, message);
         }
 
+        public static void Trash(int days = 30)
+        {
+            string path = Path.Combine(AppPath, "logs");
+            var di = new DirectoryInfo(path);
+            var fi = di.GetFiles("*.log");
+
+            for (int i = 0; i < fi.Length; i++)
+            {
+                var diff = DateTime.Compare(DateTime.Now, fi[i].CreationTimeUtc);
+                if (diff >= days) File.Delete(fi[i].FullName);
+            }
+        }
+
         private static void writeLog(LogType logType, params string[] message)
         {
             CultureInfo cultureInfo = new CultureInfo("en-US");
-            string output = Path.Combine(AppPath, string.Format("{0}_{1}.log", logType.ToString(), DateTime.Now.ToString("yyyyMMdd", cultureInfo)));
+
+            string path = Path.Combine(AppPath, "logs");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string output = Path.Combine(path, string.Format("{0}_{1}.log", logType.ToString(), DateTime.Now.ToString("yyyyMMdd", cultureInfo)));
 
             using (StreamWriter sw = new StreamWriter(new FileStream(output, FileMode.Append)))
             {
                 try
                 {
-                    sw.WriteLine("{0} {1}", DateTime.Now.ToString(), message);
+                    sw.WriteLine("{0} {1}", DateTime.Now.ToString(), string.Join(" | ", message.ToArray()));
                 }
                 catch { }
             }
